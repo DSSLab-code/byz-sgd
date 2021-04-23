eba72ad59355ed946b565a17a628df7beea7f636
YS <60769116+DSSLab-code@users.noreply.github.com>
2021-04-23 02:08:16 -0400

    Update test_zeno_1.sh

--- test_zeno_1.sh
+++ test_zeno_1.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-export MXNET_CUDNN_AUTOTUNE_DEFAULT=0
+#export MXNET_CUDNN_AUTOTUNE_DEFAULT=0
 
-export CUDA_VISIBLE_DEVICES=1
+#export CUDA_VISIBLE_DEVICES=1
 
 python mxnet_cnn_cifar10_impl.py --nepochs 2 --lr 0.05 --batch_size 100 --nworkers 16 --nbyz 0 --byz_type bitflip --rho 200 --b 12 --zeno_size 4 --aggregation mean
